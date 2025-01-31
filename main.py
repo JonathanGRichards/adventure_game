ee22d818c82b4aeb8ad2d516ac32309b99cabce4
JonathanGRichards <110139851+JonathanGRichards@users.noreply.github.com>
2025-01-31 20:18:02 +0000

    Task 1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from room import Room
 from character import Enemy
 
+# Create rooms for the setting
 kitchen = Room("kitchen")
 kitchen.description = "A dank and dirty room buzzing with flies"
 ballroom = Room("ballroom")
@@ -13,11 +14,40 @@ dining_hall.link_room(kitchen, "north")
 dining_hall.link_room(ballroom,"west")
 ballroom.link_room(dining_hall,"east")
 
+# Add inhabitants to rooms
+
+dave = Enemy("Dave", "A smelly zombie")
+dining_hall.set_character(dave)
+dave.conversation = "Hi my name is Dave, Id like to eat your brain."
+dave.weakness= "cheese"
+
+
 current_room = dining_hall
 while True:
       print("\n")
       current_room.get_details()
+      inhabitant = current_room.get_character()
       command = input("> ")
-      current_room = current_room.move(command)
+      # Check whether a direction was typed
+      if command in ["north", "south", "east", "west"]:
+            current_room = current_room.move(command)
+      elif command == "talk":
+            if inhabitant is not None:
+                  inhabitant.talk()
+            else:
+                  print("There is no one here to talk to")
+      elif command == "fight":
+            if inhabitant is not None and isinstance(inhabitant, Enemy):
+                  # Fight with the inhabitant, if there is one
+                  print("What will you fight with?")
+                  fight_with = input("\n> ")
+                  if inhabitant.fight(fight_with) == True:
+                        # What happens if you win?
+                        print("Hooray, you won the fight!")
+                        current_room.set_character(None)
+                  else:
+                        # What happens if you lose?
+                        print("Oh dear, you lost the fight.")
+            else:
+                  print("There is no one here to fight with")
 
-dining_hall.get_details()
